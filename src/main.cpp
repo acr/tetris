d7f6c38506560f4cbfef29665ca34462afa93931
alex <alex@soju>
2016-02-27 12:54:46 -0500

    window now appears. opengl state initialized

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ public:
   virtual void preLinkCallback(GLint shader_program) { }
 };
 
+bool handle_user_input();
+
 int main() {
   if(SDL_Init(SDL_INIT_VIDEO) != 0) {
     std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
@@ -45,12 +47,20 @@ int main() {
     return 3;
   }
 
+  glGetError();
+  dsp::printGLInfo();
+
   glEnable(GL_DEPTH_TEST);
+  dsp::checkGL();
   glDepthFunc(GL_LESS);
+  dsp::checkGL();
 
   glEnable(GL_CULL_FACE);
+  dsp::checkGL();
   glCullFace(GL_BACK);
+  dsp::checkGL();
   glFrontFace(GL_CCW);
+  dsp::checkGL();
 
   GLint shader_program;
   {
@@ -61,7 +71,58 @@ int main() {
       return 4;
     }
   }
-  
+
+  glm::mat4 ident_mat;
+  GLint proj_mat_location = glGetUniformLocation(shader_program, "P");
+  dsp::checkGL();
+  GLint view_mat_location = glGetUniformLocation(shader_program, "V");
+  dsp::checkGL();
+  GLint model_mat_location = glGetUniformLocation(shader_program, "M");
+  dsp::checkGL();
+  glUseProgram(shader_program);
+  glUniformMatrix4fv(proj_mat_location, 1, 0, glm::value_ptr(ident_mat));
+  dsp::checkGL();
+  glUniformMatrix4fv(view_mat_location, 1, 0, glm::value_ptr(ident_mat));
+  dsp::checkGL();
+  glUniformMatrix4fv(model_mat_location, 1, 0, glm::value_ptr(ident_mat));
+  dsp::checkGL();
+  dsp::printAll(shader_program);
+
+  do {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glViewport(0, 0, width, height);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    SDL_GL_SwapWindow(window);
+    SDL_Delay(20);
+  } while(handle_user_input());
 
   return 0;
+}
+
+bool handle_user_input() {
+  bool is_quit = false;
+  SDL_Event sdl_event;
+  while(SDL_PollEvent(&sdl_event)) {
+    switch(sdl_event.type) {
+    case SDL_QUIT: {
+      is_quit = true;
+      break;
+    }
+
+    case SDL_KEYUP: {
+      if(sdl_event.key.keysym.sym == SDLK_ESCAPE) {
+	is_quit = true;
+      }
+      break;
+    }
+
+    default: {
+      break;
+    }
+
+    }
+  }
+
+  return !is_quit;
 }
