da3be1ba5ac755b09c6966337a1742d0d8c34158
alex <alex@soju>
2016-02-27 19:52:38 -0500

    modified shaders to accept 2D vertices. added rectangle shape

--- shaders/shader.vert
+++ shaders/shader.vert
@@ -1,14 +1,14 @@
 #version 330
 
-in vec3 vertex_position;
+in vec2 vertex_position;
 in vec3 vertex_color;
 out vec4 interp_color;
 
-uniform mat4 P;
-uniform mat4 V;
-uniform mat4 M;
+uniform mat4 proj_mat;
+uniform mat4 view_mat;
+uniform mat4 model_mat;
 
 void main() {
-  gl_Position = P * V * M * vec4(vertex_position, 1.0);
+  gl_Position = proj_mat * view_mat * model_mat * vec4(vertex_position, 0.0, 1.0);
   interp_color = vec4(vertex_color, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include "gl_utils.h"
 
 #include <iostream>
+#include <math.h>
 
-static int width = 800;
-static int height = 600;
+static int width = 512;
+static int height = 1024;
+
+bool handle_user_input();
 
 class PLC : public dsp::PreLinkCallback {
 public:
@@ -12,7 +15,167 @@ public:
   virtual void preLinkCallback(GLint shader_program) { }
 };
 
-bool handle_user_input();
+class Square {
+private:
+  GLuint model_vao;
+  GLint model_uniform_location;
+  GLuint vertex_vbo;
+  GLuint color_vbo;
+  glm::vec2 position;
+  glm::vec3 color;
+
+  void init_vao() {
+    glGenBuffers(1, &vertex_vbo);
+    dsp::checkGL();
+    glGenBuffers(1, &color_vbo);
+    dsp::checkGL();
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo);
+    dsp::checkGL();
+
+    float triangles[] = {
+      -0.5f, -0.5f,
+      0.5f, 0.5f,
+      -0.5f, 0.5f,
+
+      -0.5f, -0.5f,
+      0.5f, -0.5f,
+      0.5f, 0.5f,
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
+    dsp::checkGL();
+
+    glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
+    dsp::checkGL();
+    
+    float colors[] = {
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+    };      
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    dsp::checkGL();
+
+    glGenVertexArrays(1, &model_vao);
+    dsp::checkGL();
+    glBindVertexArray(model_vao);
+    dsp::checkGL();
+    glEnableVertexAttribArray(0);
+    dsp::checkGL();
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo);
+    dsp::checkGL();
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    dsp::checkGL();
+    glEnableVertexAttribArray(1);
+    dsp::checkGL();
+    glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    dsp::checkGL();
+  }
+
+public:
+  explicit Square(GLint mul) : model_uniform_location(mul) { init_vao(); }
+  Square(GLint mul, const glm::vec2& p, const glm::vec3& c) :
+    model_uniform_location(mul), position(p), color(c) { init_vao(); }
+
+  void render() {
+    glm::mat4 model_mat = glm::translate(glm::mat4(), glm::vec3(position, 0.0f));
+    glUniformMatrix4fv(model_uniform_location, 1, 0, glm::value_ptr(model_mat));
+    dsp::checkGL();
+    glBindVertexArray(model_vao);
+    dsp::checkGL();
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    dsp::checkGL();
+  }
+};
+
+class Rectangle {
+  GLint model_uniform_location;
+  GLuint model_vao;
+  GLuint vertex_vbo;
+  GLuint color_vbo;
+  glm::vec2 lower_left;
+  glm::vec2 upper_right;
+  glm::vec3 color;
+  
+  void init_vao() {
+    glGenBuffers(1, &vertex_vbo);
+    dsp::checkGL();
+    glGenBuffers(1, &color_vbo);
+    dsp::checkGL();
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo);
+    dsp::checkGL();
+
+    float triangles[] = {
+      lower_left.x, lower_left.y,
+      upper_right.x, lower_left.y,
+
+      upper_right.x, lower_left.y,
+      upper_right.x, upper_right.y,
+
+      upper_right.x, upper_right.y,
+      lower_left.x, upper_right.y,
+
+      lower_left.x, upper_right.y,
+      lower_left.x, lower_left.y
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
+    dsp::checkGL();
+
+    glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
+    dsp::checkGL();
+
+    float colors[] = {
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+      color.r, color.g, color.b,
+      color.r, color.g, color.b
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    dsp::checkGL();
+
+    glGenVertexArrays(1, &model_vao);
+    dsp::checkGL();
+    glBindVertexArray(model_vao);
+    dsp::checkGL();
+    glEnableVertexAttribArray(0);
+    dsp::checkGL();
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo);
+    dsp::checkGL();
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    dsp::checkGL();
+    glEnableVertexAttribArray(1);
+    dsp::checkGL();
+    glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    dsp::checkGL();
+  }
+
+public:
+  Rectangle(GLint mul, const glm::vec2& ll, const glm::vec2& ur, const glm::vec3& c) :
+    model_uniform_location(mul), lower_left(ll), upper_right(ur), color(c) { init_vao(); }
+
+  void render() {
+    glm::mat4 model_mat;
+    glUniformMatrix4fv(model_uniform_location, 1, 0, glm::value_ptr(model_mat));
+    dsp::checkGL();
+    glBindVertexArray(model_vao);
+    dsp::checkGL();
+    glDrawArrays(GL_LINES, 0, 8);
+    dsp::checkGL();
+  }
+
+};
 
 int main() {
   if(SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -29,7 +192,7 @@ int main() {
 					SDL_WINDOWPOS_UNDEFINED,
 					width,
 					height,
-					SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+					SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
   if(window == 0) {
     std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
@@ -73,25 +236,37 @@ int main() {
   }
 
   glm::mat4 ident_mat;
-  GLint proj_mat_location = glGetUniformLocation(shader_program, "P");
+  glm::mat4 view_mat = glm::translate(glm::mat4(), glm::vec3(-0.5f, -1.0f, -3.0f));
+  glm::mat4 proj_mat = glm::perspective(
+    glm::radians(45.0f),
+    static_cast<float>(width) / static_cast<float>(height),
+    0.1f, 100.0f);
+
+  GLint proj_mat_location = glGetUniformLocation(shader_program, "proj_mat");
   dsp::checkGL();
-  GLint view_mat_location = glGetUniformLocation(shader_program, "V");
+  GLint model_mat_location = glGetUniformLocation(shader_program, "model_mat");
   dsp::checkGL();
-  GLint model_mat_location = glGetUniformLocation(shader_program, "M");
+  GLint view_mat_location = glGetUniformLocation(shader_program, "view_mat");
   dsp::checkGL();
   glUseProgram(shader_program);
-  glUniformMatrix4fv(proj_mat_location, 1, 0, glm::value_ptr(ident_mat));
+  glUniformMatrix4fv(proj_mat_location, 1, GL_FALSE, glm::value_ptr(proj_mat));
   dsp::checkGL();
-  glUniformMatrix4fv(view_mat_location, 1, 0, glm::value_ptr(ident_mat));
+  glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, glm::value_ptr(view_mat));
   dsp::checkGL();
-  glUniformMatrix4fv(model_mat_location, 1, 0, glm::value_ptr(ident_mat));
+  glUniformMatrix4fv(model_mat_location, 1, GL_FALSE, glm::value_ptr(ident_mat));
   dsp::checkGL();
   dsp::printAll(shader_program);
 
+
+  // Square s(model_mat_location, glm::vec2(0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+  Rectangle r(model_mat_location, glm::vec2(0.0f, 0.0f), glm::vec2(1.0f, 2.0f), glm::vec3(0.0f, 1.0f, 0.0f));
   do {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glViewport(0, 0, width, height);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    // s.render();
+    r.render();
 
     SDL_GL_SwapWindow(window);
     SDL_Delay(20);
